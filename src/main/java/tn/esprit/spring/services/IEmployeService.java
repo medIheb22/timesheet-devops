54ebac7cb49749b61291ef89117e6962f7d7f0c1
mediheb22 <nefzi.mediheb@esprit.tn>
2021-10-16 16:37:35 +0100

    first

---
+++ src/main/java/tn/esprit/spring/services/IEmployeService.java
@@ -0,0 +1,26 @@
+package tn.esprit.spring.services;
+
+import java.util.Date;
+import java.util.List;
+
+import tn.esprit.spring.entities.Contrat;
+import tn.esprit.spring.entities.Employe;
+import tn.esprit.spring.entities.Entreprise;
+
+
+public interface IEmployeService {
+	
+	public int ajouterEmploye(Employe employe);
+	public void mettreAjourEmailByEmployeId(String email, int employeId);
+	public void affecterEmployeADepartement(int employeId, int depId);
+	public void desaffecterEmployeDuDepartement(int employeId, int depId);
+	public void affecterContratAEmploye(int contratId, int employeId);
+	public String getEmployePrenomById(int employeId);
+	public void deleteEmployeById(int employeId);
+	public int getNombreEmployeJPQL();
+	public List<String> getAllEmployeNamesJPQL();
+	public List<Employe> getAllEmployeByEntreprise(Entreprise entreprise);
+	public void mettreAjourEmailByEmployeIdJPQL(String email, int employeId);
+	public float getSalaireByEmployeIdJPQL(int employeId);
+	public List<Employe> getAllEmployes();
+}
